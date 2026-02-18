833268d16edc90e853a7b05ef4e7f2833855127d
gnuipf <inaudivelporfavor@gmail.com>
2026-02-18 17:58:25 -0300

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,19 +1,5 @@
-fetch("./components/navbar.html")
+fetch("/inaudivel_por_favor/components/navbar.html")
   .then(res => res.text())
   .then(data => {
-    document.getElementById("navbar").innerHTML = data;
-
-    const base = window.location.origin + "/inaudivel_por_favor/";
-
-    const logo = document.getElementById("logoNav");
-    if (logo) {
-      logo.src = base + "img/logo.png";
-    }
-
-    
-  })
-  .catch(err => console.error("Erro carregando navbar:", err));
-
-
-
-
+    document.getElementById("navbar-container").innerHTML = data;
+  });
