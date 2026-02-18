2dd1eec9fda272cc7028520c873a8ab491de1a23
gnuipf <inaudivelporfavor@gmail.com>
2026-02-18 18:01:52 -0300

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,9 @@
 fetch("/inaudivel_por_favor/components/navbar.html")
-  .then(res => res.text())
+  .then(response => {
+    if (!response.ok) throw new Error("Navbar não encontrada");
+    return response.text();
+  })
   .then(data => {
-    document.getElementById("navbar-container").innerHTML = data;
-  });
+    document.getElementById("navbar").innerHTML = data;
+  })
+  .catch(error => console.error("Erro ao carregar navbar:", error));
