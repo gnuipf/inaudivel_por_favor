24058d86e5ea4e246ee4a4e1e78f4a121a4d99cc
gnuipf <inaudivelporfavor@gmail.com>
2026-02-18 17:26:41 -0300

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,7 @@
-fetch("/components/navbar.html")
+fetch("./components/navbar.html")
   .then(res => res.text())
   .then(data => {
     document.getElementById("navbar").innerHTML = data;
   })
   .catch(err => console.error("Erro carregando navbar:", err));
+
